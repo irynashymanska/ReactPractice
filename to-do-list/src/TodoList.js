b16b5614905484d69b49c9731b08b6f8d4bbcd71
irynashymanska <shymanska04@gmail.com>
2023-05-17 15:00:58 +0300

    commit

--- to-do-list/src/TodoList.js
+++ to-do-list/src/TodoList.js
@@ -9,12 +9,36 @@ import { FaTimes } from 'react-icons/fa';
 <AiOutlineDelete size={24} color="white" />
 
 function TodoList() {
-    const [todos, setTodos] = useState([]);
+   
     const [inputValue, setInputValue] = useState("");
     const [editingId, setEditingId] = useState(null);
     const [editValue, setEditValue] = useState("");
     const [editInputValue, setEditInputValue] = useState("");
-
+    //const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || []);
+    const [todos, setTodos] = useState([
+        {
+          id: uuid(),
+          text: "Buy milk",
+          date: new Date(),
+          pinned: false,
+          completed: false,
+        },
+        {
+          id: uuid(),
+          text: "Walk the dog",
+          date: new Date(),
+          pinned: true,
+          completed: false,
+        },
+        {
+          id: uuid(),
+          text: "Do laundry",
+          date: new Date(),
+          pinned: false,
+          completed: true,
+        },
+      ]);
+    
     function handleSubmit(e) {
         e.preventDefault();
         const newTodo = {
@@ -25,20 +49,18 @@ function TodoList() {
             completed: false,
         };
         setTodos([...todos, newTodo]);
+        localStorage.setItem('todos', JSON.stringify([...todos, newTodo]));
         setInputValue("");
+        alert("New task was added!");
     }
 
     function handleDelete(id) {
         setTodos(todos.filter((todo) => todo.id !== id));
+        localStorage.setItem('todos', JSON.stringify(todos.filter((todo) => todo.id !== id)));
+        alert("Task was deleted!");
     }
 
-    function handlePin(id) {
-        setTodos(
-            todos.map((todo) =>
-                todo.id === id ? { ...todo, pinned: !todo.pinned } : todo
-            )
-        );
-    }
+
 
     function handleCompleted(id) {
         setTodos(
@@ -46,8 +68,24 @@ function TodoList() {
                 todo.id === id ? { ...todo, completed: !todo.completed } : todo
             )
         );
+        localStorage.setItem('todos', JSON.stringify(todos));
     }
-     
+    function handlePinToTop(id) {
+        const pinnedTodo = todos.find((todo) => todo.id === id);
+        const nonPinnedTodos = todos.filter((todo) => todo.id !== id);
+        if ( pinnedTodo.pinned) {
+            pinnedTodo.pinned = false;
+            setTodos([...nonPinnedTodos, pinnedTodo]);
+            localStorage.setItem('todos', JSON.stringify([...nonPinnedTodos, pinnedTodo]));
+        }
+        else { 
+            pinnedTodo.pinned = true;
+            setTodos([pinnedTodo, ...nonPinnedTodos]);
+            localStorage.setItem('todos', JSON.stringify([pinnedTodo, ...nonPinnedTodos]));
+        }
+
+       
+      }
     function handleSaveEdit(e) {
         e.preventDefault();
         setTodos(
@@ -57,6 +95,9 @@ function TodoList() {
         );
         setEditingId(null);
         setEditInputValue("");
+        localStorage.setItem('todos', JSON.stringify( todos.map((todo) =>
+        todo.id === editingId ? { ...todo, text: editInputValue } : todo
+      )));
     }
 
     function handleEdit(id) {
@@ -72,6 +113,9 @@ function TodoList() {
                 todo.id === editingId ? { ...todo, text: editValue } : todo
             )
         );
+        localStorage.setItem('todos', JSON.stringify(todos.map((todo) =>
+        todo.id === editingId ? { ...todo, text: editValue } : todo
+    )));
         setEditingId(null);
         setEditValue("");
     }
@@ -84,51 +128,60 @@ function TodoList() {
 
     return (
         <div >
-            <form onSubmit={handleSubmit} class="createInput mt-5 mb-5 d-flex justify-content-center">
-                <span class="align-middle"><input
+            <form onSubmit={handleSubmit} className="createInput mt-5 mb-5 d-flex justify-content-center">
+                <span className="align-middle"><input
                     type="text"
                     placeholder="Add new todo"
                     value={inputValue}
-                    class="form-control d-inline"
+                    className="form-control d-inline"
                     onChange={(e) => setInputValue(e.target.value)}
                 /></span>
-                <button type="submit" class="btn btn-primary ms-4">Create</button>
+                <button type="submit" className="btn btn-primary ms-4">Create</button>
             </form>
             {todos.map((todo) => (
-                <div key={todo.id} class="createInput mt-2 mb-2 d-flex justify-content-center">
-
-                    <span class="form-check align-middle p-2">
-                        <input class="form-check-input" type="checkbox"
+                <div key={todo.id} className="createInput mt-2 mb-2 d-flex justify-content-center container" >
+                     <div  className="col-md-2">
+                    
+                
+                  </div>
+        <div  className="col-md-5">
+                    <p className="form-check align-middle p-1">
+                        <input className="form-check-input" type="checkbox"
                             checked={todo.completed}
                             onChange={() => handleCompleted(todo.id)} />
-                        <label class="form-check-label text-light">
+                        <label className="form-check-label text-light">
                             {todo.completed ? <span style={{ textDecoration: 'line-through' }}>{todo.text }</span> : <span>{todo.text }</span>}
                         </label>
-                    </span>
-
-                    <span class="text-light p-2">{todo.date.toLocaleDateString()}</span>
-                    <button onClick={() => handlePin(todo.id)} class="btn btn-secondary ms-2">
-                        {todo.pinned ?  <RiPushpin2Fill /> : <RiPushpin2Line />}
+                     
+                    </p>
+                
+                  </div>
+                   
+                    <div className="col-md-5">
+                    <span className="text-light p-2">{todo.date.toLocaleDateString()}</span>
+                    <button onClick={() => handlePinToTop(todo.id)} className="btn btn-secondary ms-2">
+                    {todo.pinned ? <RiPushpin2Fill /> : <RiPushpin2Line />}
                     </button>
-                    <button onClick={() => handleEdit(todo.id)} class="btn btn-primary ms-2"><FaEdit /> </button>
-                    <button onClick={() => handleDelete(todo.id)} class="btn btn-danger ms-2"><AiOutlineDelete /></button>
+                    <button onClick={() => handleEdit(todo.id)} className="btn btn-primary ms-2"><FaEdit /> </button>
+                    <button onClick={() => handleDelete(todo.id)} className="btn btn-danger ms-2"><AiOutlineDelete /></button>
                     {editingId === todo.id ? (
-                    <form onSubmit={handleEditSubmit} class="createInput d-flex justify-content-center">
+                    <form onSubmit={handleEditSubmit} className="createInput mt-2 d-flex justify-content-center">
                         <input
                             type="text"
                             placeholder="Edit todo"
                             value={editInputValue}
-                            class="form-control d-inline w-50"
+                            className="form-control d-inline w-50"
                             onChange={(e) => setEditInputValue(e.target.value)}
                         />
-                        <button  onClick={handleSaveEdit} type="submit"  class="btn btn-primary ms-2"><FaSave/></button>
-                        <button onClick={handleCancelEdit} class="btn btn-secondary ms-2"><FaTimes /></button>
+                        <button  onClick={handleSaveEdit} type="submit"  className="btn btn-primary ms-2"><FaSave/></button>
+                        <button onClick={handleCancelEdit} className="btn btn-secondary ms-2"><FaTimes /></button>
                     </form>
                 ) : (
                     <>
                        
                     </>
                 )}
+            </div>
             </div>
 
             ))}
